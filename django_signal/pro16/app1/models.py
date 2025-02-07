1812b2c6a09dd6ee6a9db397b181cfce65ec7ac8
gangatharan2 <gangakaran08082004@gmail.com>
2025-02-07 13:08:48 +0530

    Delete django_signal directory
    delete successfull

--- django_signal/pro16/app1/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class MyModel(models.Model):
-    name = models.CharField(max_length=255)
-
-class OtherModel(models.Model):
-    related_model = models.ForeignKey(MyModel, on_delete=models.CASCADE)
